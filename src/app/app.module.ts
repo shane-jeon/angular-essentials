7772e435113ce78bd04dcf92a4493c3064486af0
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2024-10-27 14:29:51 -0700

    replaced property binding with two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { UserComponent } from './user.component';
 
 @NgModule({
   declarations: [AppComponent, UserComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" (input)="onUserInput($event)" [value]="name" />
+    <input type="text" [(ngModel)]="name" />
     <p>Hello {{ name }}!</p>
     <p>I'm the user component!</p>
   `,
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 export class UserComponent {
   name = 'Shane';
 
-  onUserInput(event) {
-    this.name = event.target.value;
-  }
+  // onUserInput(event) {
+  //   this.name = event.target.value;
+  // }
 }
